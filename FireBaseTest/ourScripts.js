b6c00b38d50e85fd846aadcf76132f8f430f950d
jiedua1 <jiedongduan@gmail.com>
2018-02-10 17:13:40 -0600

    Added some json objects

--- FireBaseTest/ourScripts.js
+++ FireBaseTest/ourScripts.js
@@ -2,5 +2,27 @@ var textBox = document.getElementById("textBox");
 
 function submit() {
   var fireBaseRef = firebase.database().ref();
-  fireBaseRef.child("userWords").set(textBox.value);
+  if(textBox.value !== "") {
+    fireBaseRef.child("imageURL").set(textBox.value);
+  }
 }
+
+var playersRef = firebase.database().ref("players/");
+playersRef.set ({
+   John: {
+      number: 1,
+      age: 30,
+      children: {
+        child1: "Bob"
+      }
+   },
+
+   Amanda: {
+      number: 2,
+      age: 20,
+      children: {
+        child1: "Amanda Jr.",
+        child2: "Billy"
+      }
+   }
+});
